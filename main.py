1da06bbf95b149075d7bbfe19cf068518ce19503
Felix <sidler81@googlemail.com>
2021-03-20 13:28:10 +0100

    init

---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras import Input
+from tensorflow.python.keras.layers import Dense, GaussianNoise
+from tensorflow.python.keras.models import Model
+
+DATA_DIR = os.path.join("E:/PycharmProjects/VMT-AI/data/")
+MODELS_CKPT_DIR = os.path.join("E:/PycharmProjects/VMT-AI/models/vmt.ckpt/")
+
+
+def build_model(train_set: pd.DataFrame) -> Model:
+    act_func = 'elu'
+    encoding_dim = 20
+    features = train_set.shape[1]
+    inp = Input(shape=(features,))
+    x = GaussianNoise(stddev=0.3)(inp)
+    x = Dense(encoding_dim * 2, activation=act_func)(x)
+    out = Dense(encoding_dim * 2, activation=act_func)(x)
+    out = Dense(features, activation=act_func)(x)
+    model = Model(inputs=[inp], outputs=[out])
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss="mae")
+    model.summary()
+    return model
+
+
+def getdata(filename: str):
+    merged_data = pd.DataFrame()
+    msg = pd.read_csv(DATA_DIR + filename)
+    merged_data = merged_data.append(msg)
+    merged_data.columns = ["110", "115", "120"]
+    dataset_train = merged_data[3:]
+    dataset_test = merged_data[:2]
+    x_train = pd.DataFrame(dataset_train,
+                           columns=dataset_train.columns,
+                           index=dataset_train.index)
+    x_test = pd.DataFrame(dataset_test,
+                          columns=dataset_test.columns,
+                          index=dataset_test.index)
+    return x_train, x_test
+
+
+def handle_model_callback(model: Model):
+    model.save('./models/vmt_anomaly.h5')
+    model.save_weights('./models/vmt_anomaly_weights.h5')
+
+
+if __name__ == '__main__':
+    tf_callback = tf.keras.callbacks.ModelCheckpoint(MODELS_CKPT_DIR, save_freq=500, monitor="loss",
+                                                     save_best_only=True, verbose=1)
+    # tb_callback = tf.keras.callbacks.TensorBoard(log_dir="models/logs", histogram_freq=10, write_graph=True,
+    #                                             write_images=True, update_freq="epoch", embeddings_freq=10)
+    X_train, X_test = getdata("test.csv")
+    vmt_model = build_model(X_train)
+    if os.path.exists(MODELS_CKPT_DIR):
+        vmt_model = tf.keras.models.load_model(MODELS_CKPT_DIR)
+    vmt_model.fit(np.array(X_train), np.array(X_train), batch_size=32, verbose=1, epochs=2000, validation_split=0.1,
+                  callbacks=[tf_callback])
+    handle_model_callback(vmt_model)
---
+++ predict.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import datetime
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+vmt_model = tf.keras.models.load_model("models/vmt_anomaly.h5")
+
+
+def predict(predict_data, model) -> pd.DataFrame():
+    pred = model.predict(np.array(predict_data))
+    pred = pd.DataFrame(pred, columns=predict_data.columns)
+    pred.index = predict_data.index
+    scored = pd.DataFrame(index=predict_data.index)
+    scored['Error_Rate'] = np.mean(np.abs(pred - predict_data), axis=1)
+    scored['Threshold'] = 1
+    scored['Anomaly'] = scored['Threshold'] < scored['Error_Rate']
+    return scored
+
+
+@app.route('/', methods=['POST'])
+def index():
+    pd.read_csv(request.files['csv']).to_csv("data/" + str(datetime.date.today()) + ".csv", index=False)
+    resp = predict(pd.read_csv("data/" + str(datetime.date.today()) + ".csv"), vmt_model)
+    resp = resp.drop(['Threshold'], 1)
+    return jsonify(resp.to_dict(orient="index"))
+
+
+if __name__ == '__main__':
+    app.run()
